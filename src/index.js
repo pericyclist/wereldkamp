ab491a9453e9f33f87571d0bd71edaed6918e779
pericyclist <tom.ysewijn@gmail.com>
2025-11-28 16:00:50 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app.js'; // Let op: importeer hier uw app.js
+
+// Zoek de root DOM-node in public/index.html
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Render de hoofdcomponent (App) in de root
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Als u een apart CSS-bestand zou gebruiken (niet nodig met Tailwind):
+// import './index.css';
